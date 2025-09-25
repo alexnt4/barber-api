2b0c710bb474234c5d14c8d4bc5631e5f76c23d1
Alex <kend1123ue@gmail.com>
2025-09-24 22:17:25 -0500

    feat: add appointments endpoints

--- internal/transport/http/appointment_handler.go
+++ internal/transport/http/appointment_handler.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/alexnt4/barber-api/internal/domain"
@@ -82,5 +83,118 @@ func (h *AppointmentHandler) List(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"appoinmets": appoinmets,
 		"total":      len(appoinmets),
+	})
+}
+
+func (h *AppointmentHandler) Get(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.ParseUint(idStr, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID invalido"})
+		return
+	}
+
+	appt, err := h.svc.GetById(c.Request.Context(), uint(id))
+	if err != nil {
+		if err == domain.ErrorNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "cita no encontrada"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, appt)
+}
+
+func (h *AppointmentHandler) Update(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.ParseUint(idStr, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID invalido"})
+		return
+	}
+
+	var req UpdateAppointmentRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Parseo de fechas
+	startTime, err := time.Parse(time.RFC3339, req.StartTime)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "formato de fecha invalido para start_time, use RFC3339"})
+	}
+
+	endTime, err := time.Parse(time.RFC3339, req.EndTime)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "formato de fecha invalido para end_time, use RFC3339"})
+		return
+	}
+
+	// Mapear IDs a domain.Product
+	products := make([]domain.Product, len(req.Products))
+	for i, id := range req.Products {
+		products[i] = domain.Product{ID: id}
+	}
+
+	appt := &domain.Appointment{
+		ID:          uint(id),
+		ClienteName: req.ClienteName,
+		StartTime:   startTime,
+		EndTime:     endTime,
+		Products:    products,
+	}
+
+	if err := h.svc.Update(c.Request.Context(), uint(id), appt); err != nil {
+		c.JSON(http.StatusConflict, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, appt)
+}
+
+func (h *AppointmentHandler) Delete(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.ParseUint(idStr, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID invalido"})
+		return
+	}
+
+	if err := h.svc.Cancel(c.Request.Context(), uint(id)); err != nil {
+		if err == domain.ErrorNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "cita no encontrada"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "cita cancelada exitosamente"})
+}
+
+func (h *AppointmentHandler) GetTotal(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.ParseUint(idStr, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID invalido"})
+		return
+	}
+
+	total, err := h.svc.GetTotalPrice(c.Request.Context(), uint(id))
+	if err != nil {
+		if err == domain.ErrorNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "cita no encontrada"})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"appoinmet_id": id,
+		"total":        total,
 	})
 }
