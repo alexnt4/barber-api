886fe0957f4d0086ea2cd8eb3af2e67c02dbe27c
Alex <kend1123ue@gmail.com>
2025-09-27 16:48:35 -0500

    feat: drop tables migration

--- migrations/001_create_tables.down.sql
+++ migrations/001_create_tables.down.sql
@@ -1,1 +1,10 @@
-
+--  Elminar índices 
+DROP INDEX IF EXISTS idx_products_name;
+DROP INDEX IF EXISTS idx_appointments_end_time;
+DROP INDEX IF EXISTS idx_appointmets_start_time;
+-- Eliminar tabla de relacion many-to-many
+DROP TABLE IF EXISTS appoinments_products;
+-- Eliminar tabla de citas
+DROP TABLE IF EXISTS appointmets;
+-- Eliminar tabla de productos
+DROP TABLE IF EXISTS products;
